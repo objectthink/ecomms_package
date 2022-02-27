2947395a63b4e8986a38761bef8d5dd87734f961
stephen eshelman, objectthink <stephen.n.eshelman@objectthink.com>
2022-02-27 10:46:53 -0500

    add ants dependency

--- Sources/ecomms_package/ecomms_package.swift
+++ Sources/ecomms_package/ecomms_package.swift
@@ -1,6 +1,26 @@
-public struct ecomms_package {
-    public private(set) var text = "Hello, World!"
+import SwiftyNats
 
-    public init() {
-    }
+public struct ecomms_package {
+   public private(set) var text = "Hello, World!"
+   
+   public init() {
+   }
+   
+   public func connect() -> Bool {
+
+      var client: NatsClient?
+      
+      // register a new client
+      client = NatsClient("http://192.168.86.31:4222")
+      
+      // listen to an event
+      client!.on(NatsEvent.connected) { _ in
+         print("Client connected")
+      }
+      
+      // try to connect to the server
+      try? client!.connect()
+      
+      return true
+   }
 }
