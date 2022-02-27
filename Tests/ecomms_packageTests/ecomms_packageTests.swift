4669c2c638f9178ceabf31515180518a0d59fb05
stephen eshelman, objectthink <stephen.n.eshelman@objectthink.com>
2022-02-27 09:50:37 -0500

    add another test

--- Tests/ecomms_packageTests/ecomms_packageTests.swift
+++ Tests/ecomms_packageTests/ecomms_packageTests.swift
@@ -14,4 +14,13 @@ final class ecomms_packageTests: XCTestCase {
        // Use XCTAssert and related functions to verify your tests produce the correct
        // results.
        XCTAssertEqual(ecomms_package().text, "hello cruel world")
-   }}
+   }
+
+   func testConnect() throws {
+       // This is an example of a functional test case.
+       // Use XCTAssert and related functions to verify your tests produce the correct
+       // results.
+       XCTAssertEqual(ecomms_package().text, "hello cruel world")
+   }
+
+}
